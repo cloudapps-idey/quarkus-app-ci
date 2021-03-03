04d24eb978f12c98df3b130766e629c063cce5e2
cloudapps-idey <IndraniDey@live.com>
2021-03-03 13:22:54 -0500

    initial

---
+++ build/create-tasks.sh
@@ -0,0 +1,14 @@
+oc create --save-config=true -f image-streams/imageStream-quarkus-app.yaml
+oc create --save-config=true -f persistentVolumes/task-cache-pvc.yaml 
+oc create --save-config=true -f resources/imageStreamResource-intermediate.yaml 
+oc create --save-config=true -f resources/imageStreamResource-final.yaml 
+oc create --save-config=true -f resources/sourceCode-GitResource.yaml 
+oc create --save-config=true -f resources/deployCode-GitResource.yaml
+oc create --save-config=true -f tasks/build.yaml 
+oc create --save-config=true -f tasks/clearBuildahRepo.yaml 
+oc create --save-config=true -f tasks/clearResources.yaml 
+oc create --save-config=true -f tasks/createRuntimeImage.yaml 
+oc create --save-config=true -f tasks/ocProcessDeploymentTemplate.yaml 
+oc create --save-config=true -f tasks/pushImageToQuay.yaml
+oc create --save-config=true -f pipelines/pipeline.yaml
+oc create --save-config=true -f quay-secret.yaml
